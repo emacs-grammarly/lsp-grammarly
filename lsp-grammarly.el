d7d4c1c3ad690f268b435fdcf43b67f257533975
JenChieh <jcs090218@gmail.com>
2021-02-20 00:49:21 +0800

    Update core

--- lsp-grammarly.el
+++ lsp-grammarly.el
@@ -27,7 +27,7 @@
 
 ;;; Commentary:
 ;;
-;; Grammarly Client settings
+;; LSP server implementation for Grammarly
 ;;
 
 ;;; Code:
@@ -36,10 +36,26 @@
 
 (defgroup lsp-grammarly nil
   "Settings for the Grammarly Language Server."
-  :tag "Language Server"
-  :group 'lsp-mode)
+  :group 'lsp-mode
+  :link '(url-link "https://github.com/znck/grammarly"))
 
+(defun lsp-grammarly--server-command ()
+  "Generate startup command for Grammarly language server."
+  (list (lsp-package-path 'grammarly-ls) "--stdio"))
 
+(lsp-dependency 'grammarly-ls
+                '(:system "grammarly-ls")
+                '(:npm :package "unofficial-grammarly-language-server"
+                       :path "unofficial-grammarly-language-server"))
+
+(lsp-register-client
+ (make-lsp-client
+  :new-connection (lsp-stdio-connection #'lsp-grammarly--server-command)
+  :major-modes '(text-mode latex-mode org-mode markdown-mode)
+  :priority -1
+  :server-id 'grammarly-ls
+  :download-server-fn (lambda (_client callback error-callback _update?)
+                        (lsp-package-ensure 'grammarly-ls callback error-callback))))
 
 (provide 'lsp-grammarly)
 ;;; lsp-grammarly.el ends here
