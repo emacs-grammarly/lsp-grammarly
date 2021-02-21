502cdde099271f68ce4b15e7ed5692bfafdaddfd
JenChieh <jcs090218@gmail.com>
2021-02-21 16:30:04 +0800

    Add lsp-mode to dep

--- lsp-grammarly.el
+++ lsp-grammarly.el
@@ -7,7 +7,7 @@
 ;; Description: LSP Clients for Grammarly.
 ;; Keyword:
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "24.3") (lsp-mode "6.1"))
 ;; URL: https://github.com/emacs-grammarly/lsp-grammarly
 
 ;; This file is NOT part of GNU Emacs.
