aa220addc5e425c222e9851e134ebfe4bc5605ac
JenChieh <jcs090218@gmail.com>
2021-03-08 00:29:31 +0800

    Bump version

--- lsp-grammarly.el
+++ lsp-grammarly.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: LSP Clients for Grammarly.
 ;; Keyword: lsp grammarly checker
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "24.3") (lsp-mode "6.1") (ht "2.3"))
 ;; URL: https://github.com/emacs-grammarly/lsp-grammarly
 
