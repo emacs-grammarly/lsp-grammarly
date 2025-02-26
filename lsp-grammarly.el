7b788f97d21d689d152c31e7876a04813391d48a
JenChieh <jcs090218@gmail.com>
2025-02-26 15:40:49 -0800

    feat: Drop support for Emacs 26.x and 27.x

--- lsp-grammarly.el
+++ lsp-grammarly.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/emacs-grammarly/lsp-grammarly
 ;; Version: 0.3.0
-;; Package-Requires: ((emacs "27.1") (lsp-mode "6.1") (grammarly "0.3.0") (request "0.3.0") (s "1.12.0") (ht "2.3"))
+;; Package-Requires: ((emacs "28.1") (lsp-mode "6.1") (grammarly "0.3.0") (request "0.3.0") (s "1.12.0") (ht "2.3"))
 ;; Keywords: convenience lsp grammarly checker
 
 ;; This file is NOT part of GNU Emacs.
